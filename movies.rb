8f9061b62d6afc06d97c86e3591dc45969da1fb3
carmen0208 <13770576@qq.com>
2016-07-19 19:24:32 +0800

    put each rows into an array and split items by comma

--- movies.rb
+++ movies.rb
@@ -1,14 +1,24 @@
-# Read the movie.csv file
+#1. Read the movie.csv file
 
 # read_all_line = lambda {|file_handle| file_handle.readlines }
-#
 # lines = File.open("movies.csv", "r",&read_all_line)
 
 lines = File.open("movies.csv", "r") do |file_handle|
   file_handle.readlines
 end
 
-p lines
-# Parse the data into a set of movies
-# Find out how many movies I have in each genre
-# Show a list of all the titles directed by George Lucas
+# "title.director..." => ["title", "director"...]
+rows = []
+lines.each do |line|
+  rows << line.chomp.split(",")
+end
+
+p rows.first
+#=> ["title", "release date", "director", "genre"]
+
+
+
+
+#2. Parse the data into a set of movies
+#3. Find out how many movies I have in each genre
+#4. Show a list of all the titles directed by George Lucas
