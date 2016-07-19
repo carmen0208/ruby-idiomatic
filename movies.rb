d224636bc8561edcc11d2f4e5e78df65ac572565
carmen0208 <13770576@qq.com>
2016-07-19 18:45:36 +0800

    the most common way that we did in other programming language

--- movies.rb
+++ movies.rb
@@ -1,4 +1,9 @@
 # Read the movie.csv file
+file_handle = File.open("movies.csv", "r")
+lines = file_handle.readlines
+file_handle.close
+
+p lines
 # Parse the data into a set of movies
 # Find out how many movies I have in each genre
 # Show a list of all the titles directed by George Lucas
