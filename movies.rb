09ac1845771fd769b46f9c73e1ce867b1205b2a6
carmen0208 <13770576@qq.com>
2016-07-19 18:52:55 +0800

    Block coding: 2 ways to impliements a block to working on that same function that we used to do in the last commit

--- movies.rb
+++ movies.rb
@@ -1,9 +1,11 @@
 # Read the movie.csv file
-file_handle = File.open("movies.csv", "r")
-begin
-lines = file_handle.readlines
-ensure
-file_handle.close
+
+# read_all_line = lambda {|file_handle| file_handle.readlines }
+#
+# lines = File.open("movies.csv", "r",&read_all_line)
+
+lines = File.open("movies.csv", "r") do |file_handle|
+  file_handle.readlines
 end
 
 p lines
