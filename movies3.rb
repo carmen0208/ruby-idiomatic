d497f10ee518d8bdc16db621616d0d6565d2a66b
carmen0208 <13770576@qq.com>
2016-07-21 11:16:15 +0800

    Select:Filter a Clloection

--- movies3.rb
+++ movies3.rb
@@ -26,7 +26,40 @@ rows= CSV.read('movies.csv', :headers => true, :header_converters => :symbol,
 
 movies = rows.map {|row| Movie.new(row) }
 
-p movies.last
+#2-b: get the rotten_tomatoe score
+## 1st way to do it :
+# with_rotten_tomatoes = []
+# movies.each do |movie|
+#   with_rotten_tomatoes << movie if movie.rotten_tomatoes > 0
+# end
+#
+
+## 2nd way to do it :
+# module Enumerable
+#   def subset
+#     collection =[]
+#     each do |item|
+#       collection << item if yield(item)
+#     end
+#     collection
+#   end
+# end
+#
+# with_rotten_tomatoes = movies.subset {|movie|  movie.rotten_tomatoes >0 }
+
+
+##3rd way to do it :
+with_rotten_tomatoes = movies.select{|movie| movie.rotten_tomatoes >0}
+total_rotten_tomatoes = 0.0
+with_rotten_tomatoes.each do |movie|
+  total_rotten_tomatoes += movie.rotten_tomatoes
+end
+
+average_rotten_tomatoes = total_rotten_tomatoes / with_rotten_tomatoes.size
+
+puts "Average Rotten Tomatoes Score: #{average_rotten_tomatoes}"
+
+# p movies.last
 
 #3. Find out how many movies I have in each genre
 #4. Show a list of all the titles directed by George Lucas
