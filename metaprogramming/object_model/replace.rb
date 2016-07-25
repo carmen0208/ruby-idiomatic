37751e175198446ee1253cb39056739bf81e6f0f
carmen0208 <13770576@qq.com>
2016-07-25 18:51:35 +0800

    Metaprogramming::Chapter2: Open Classes2, Monkey patching

---
+++ metaprogramming/object_model/replace.rb
@@ -0,0 +1,33 @@
+def replace(array, orignal, replacement)
+  array.map {|e| e==orignal ? replacement : e}
+end
+
+require "minitest/autorun"
+
+class ReplacementTest < Minitest::Test
+  def test_replace
+    original = ['one', 'two', 'one', 'three']
+    replaced = replace(original, 'one', 'zero')
+    assert_equal ['zero', 'two', 'zero', 'three'], replaced
+  end
+end
+
+# replace method has an defination in Array, it can be a risk
+class Array
+  def replace(orignal, replacement)
+    self.map { |e| e == orignal ? replacement : e}
+  end
+end
+
+class ArrayExtensionTest < Minitest::Test
+  def test_replace
+    original = ['one', 'two', 'one', 'three']
+    replaced = original.replace('one', 'zero')
+    assert_equal ['zero', 'two', 'zero', 'three'], replaced
+  end
+end
+
+# it can be dangous because you might trace other array replace method,
+# which would crasht he other function
+# Monkey patching
+puts [].methods.grep /^re/
