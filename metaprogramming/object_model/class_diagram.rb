4822a0de0c2a340bcf2c84ea700f8ca6da4ecf2c
carmen0208 <13770576@qq.com>
2016-07-26 22:12:09 +0800

    Metaprogramming::Chapter2: Open Classes2, Missing Lines about Class Diagram

--- metaprogramming/object_model/class_diagram.rb
+++ metaprogramming/object_model/class_diagram.rb
@@ -28,3 +28,12 @@ assert_equals Object, MyClass.superclass
 assert_equals Class, MyClass.class
 assert_equals Module, Class.superclass
 assert_equals Object, Module.superclass
+
+puts Object.class
+puts Module.superclass
+puts Class.class
+
+require_relative '../test/assertions'
+assert_equals Object, Module.superclass
+assert_equals Class, Class.class
+assert_equals Class, Object.class
