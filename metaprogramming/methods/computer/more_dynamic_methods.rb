16c65415ddfcb46209ddbb2d0a21f73b921ffc0f
carmen0208 <13770576@qq.com>
2016-08-01 10:46:04 +0800

    Metaprogramming::Chapter3::Method:Dynamic Dispatches and Dynamic and Generating Methods Dynamically -- I don't get  part

---
+++ metaprogramming/methods/computer/more_dynamic_methods.rb
@@ -0,0 +1,33 @@
+#---
+# Excerpted from "Metaprogramming Ruby 2",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/ppmetr2 for more book information.
+#---
+# Computer with define_method() and HWDataSource introspection
+
+require_relative 'data_source'
+
+class Computer
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+# not so clear with $1, which seems a global variable, but I don't know what it is
+    data_source.methods.grep(/^get_(.*)_info$/) { Computer.define_component $1 }
+  end
+
+  def self.define_component(name)
+    define_method(name) do
+      # ...
+      info = @data_source.send "get_#{name}_info", @id
+      price = @data_source.send "get_#{name}_price", @id
+      result = "#{name.capitalize}: #{info} ($#{price})"
+      return "* #{result}" if price >= 100
+      result
+    end
+  end
+end
+
+require_relative 'unit_test'
