bb09634a79ea52146ffc590d0b29a81d6d34b9de
carmen0208 <13770576@qq.com>
2016-08-07 17:32:49 +0800

    Metaprogramming::Chapter6::Coa that writes Code: Quiz: Checked Attributes (Step 1) - using eval

---
+++ metaprogramming/ctwc/checked_attributes/eval.rb
@@ -0,0 +1,42 @@
+require 'minitest/autorun'
+
+class Person
+end
+
+class TestCheckedAttribute < Minitest::Test
+  def setup
+    add_check_attribute(Person, :age)
+    @bob = Person.new
+  end
+
+  def test_accepts_valid_values
+    @bob.age = 20
+    assert_equal 20, @bob.age
+  end
+
+  def test_refuses_nil_values
+    assert_raises RuntimeError, 'Invalid attribute' do
+      @bob.age = nil
+    end
+  end
+  def test_refuses_false_values
+    assert_raises RuntimeError, 'Invalid attribute' do
+      @bob.age = false
+    end
+  end
+end
+
+# Here is the method that you should implement.
+def add_check_attribute(klass, attribute)
+  eval "
+    class #{klass}
+      def #{attribute}=(value)
+        raise 'Invalid attribute' unless value
+        @#{attribute} = value
+      end
+      def #{attribute}()
+        @#{attribute}
+      end
+    end
+  "
+end
